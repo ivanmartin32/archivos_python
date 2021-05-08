8692f98f95bfb3c24a35cab632ce6f7f4ca64fc9
ivanmartin32 <ivanmartin_32@hotmail.com>
2021-05-08 09:26:53 -0300

    Update ejercicios_profundizacion.py

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -15,10 +15,14 @@ __author__ = "Inove Coding School"
 __email__ = "alumnos@inove.com.ar"
 __version__ = "1.4"
 
+import csv
+import re
+from statistics import mean
+
 
 def ej1():
     print("Escrutinio de los alquileres de Capital Federal")
-    cantidad_ambientes = 2
+    #cantidad_ambientes = 2
 
     '''
     Realizar un prorgrama que solicite la cantidad de
@@ -33,8 +37,32 @@ def ej1():
     4) Obtener el mínimo valor de alquiler en "pesos"
     de la cantidad de ambientes deseados.
     '''
+    
+    with open ('propiedades.csv') as csvfile:
+        alquiler = list(csv.DictReader(csvfile))
 
+    alquiler_ARS = 0
+    promedio_alquiler_ARS = []
+    max_alquiler = []
+    min_alquiler = []
 
+    for i in range(len(alquiler)):
+        columnas = alquiler[i]
+        for k,v in columnas.items():
+            if (k == 'moneda') and (v == 'ARS'):
+                alquiler_ARS += 1
+            if k == 'precio':
+                promedio_alquiler_ARS.append(float(v))
+            if k == 'precio':
+                max_alquiler.append(float(v))
+            if k == 'precio':
+                min_alquiler.append(float(v))
+
+    print('La cantidad de alquileres en pesos son:', alquiler_ARS)
+    print('El promedio de valores de alquileres es:', mean(promedio_alquiler_ARS))
+    print('El alquiler maximo es de:', max(max_alquiler))
+    print('El alquiler minimo es de:', min(min_alquiler))
+    
 def ej2():
     print('Proyecto final!')
     '''
